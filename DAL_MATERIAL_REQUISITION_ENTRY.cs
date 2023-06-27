09b7cd2f8a9e9177fd3859fc5a9b030759f4f1db
rohitc126 <121222869+rohitc126@users.noreply.github.com>
2023-06-27 18:51:07 +0530

    Add files via upload

---
+++ DAL_MATERIAL_REQUISITION_ENTRY.cs
@@ -0,0 +1,129 @@
+﻿using BusinessLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DAL
+{
+    public class DAL_MATERIAL_REQUISITION_ENTRY
+    {
+        public string INSERT_MATERIAL_REQUISITION_ENTRY(string Emp_Code, Material_Entry MAT, out ResultMessage oblMsg)
+        {
+            string errorMsg = "";
+            oblMsg = new ResultMessage();
+         
+            using (var connection = new SqlConnection(sqlConnection.GetConnectionString_SGX()))
+            {
+                connection.Open();
+                SqlCommand command;
+                SqlTransaction transactionScope = null;
+                transactionScope = connection.BeginTransaction(IsolationLevel.ReadCommitted);
+                try
+                {
+
+                    SqlParameter[] param =
+                                    {
+                                       new SqlParameter("@ERRORSTR", SqlDbType.VarChar, 200) 
+                                       ,new SqlParameter("@REQUISITION_ID", SqlDbType.Decimal) 
+                                      ,new SqlParameter("@REQUISITION_CODE", SqlDbType.VarChar, 20)
+                                       
+                                      ,new SqlParameter("@REQUISITION_DATE", MAT.Requisition_DT)
+                                      ,new SqlParameter("@BRANCH_CODE",MAT.BRANCH_CODE)  
+                                      ,new SqlParameter("@MATERIAL_ID",MAT.Material_Id)             
+                                      ,new SqlParameter("@QUANTITY",  MAT.Qty) 
+                                      ,new SqlParameter("@UNIT", MAT.Unit)
+                                      ,new SqlParameter("@EXPECTED_DEL_DATE",MAT.Expected_DT)
+                                      ,new SqlParameter("@ADDED_BY",Emp_Code)  
+                    
+                    };
+
+                    param[0].Direction = ParameterDirection.Output;
+                    param[1].Direction = ParameterDirection.Output;
+                    param[2].Direction = ParameterDirection.Output;
+
+                    new DataAccess().InsertWithTransaction("[AGG].[USP_INSERT_MATERIAL_REQUISITION_ENTRY]", CommandType.StoredProcedure, out command, connection, transactionScope, param);
+                    decimal REQUISITION_ID = (decimal)command.Parameters["@REQUISITION_ID"].Value;
+                    string REQUISITION_CODE = (string)command.Parameters["@REQUISITION_CODE"].Value;
+                    string error_1 = (string)command.Parameters["@ERRORSTR"].Value;
+                    if (REQUISITION_ID == -1) { errorMsg = error_1; }
+
+
+
+                    if (errorMsg == "")
+                    {
+
+                        oblMsg.ID = REQUISITION_ID;
+                        oblMsg.CODE = REQUISITION_CODE;
+                        oblMsg.MsgType = "Success";
+                        transactionScope.Commit();
+
+                    }
+
+                    else
+                    {
+                        oblMsg.Msg = errorMsg;
+                        oblMsg.MsgType = "Error";
+                        transactionScope.Rollback();
+                    }
+                }
+
+                catch (Exception ex)
+                {
+
+                    errorMsg = "Error: Exception occured. " + ex.StackTrace.ToString();
+
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return errorMsg;
+        }
+
+
+        public List<MATERIAL_REQ_DATA_LIST> Select_Material_Data_List(Material_Req_List materiallist)
+        {
+            SqlParameter[] param = {  
+                                      
+                                       new SqlParameter("@BRANCH_CODE", materiallist.BRANCH_CODE),
+                                       new SqlParameter("@FROM_DT", materiallist.From_DT),
+                                       new SqlParameter("@TO_DT", materiallist.To_DT) 
+                                   };
+
+            DataSet ds = new DataAccess(sqlConnection.GetConnectionString_SGX()).GetDataSet("[AGG].[USP_SELECT_MATERIAL_REQUISITION_LIST]", CommandType.StoredProcedure, param);
+
+            List<MATERIAL_REQ_DATA_LIST> _list = new List<MATERIAL_REQ_DATA_LIST>();
+            DataTable dt = ds.Tables[0];
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    _list.Add(new MATERIAL_REQ_DATA_LIST
+                    {
+                        REQUISITION_ID = Convert.ToDecimal(row["REQUISITION_ID"]),
+                        REQUISITION_CODE = Convert.ToString(row["REQUISITION_CODE"]),
+                        REQUISITION_DATE = Convert.ToString(row["REQUISITION_DATE"]),
+                        Material_Name = Convert.ToString(row["Material_Name"]),
+                        QUANTITY = Convert.ToDecimal(row["QUANTITY"]),
+                        UNIT = Convert.ToString(row["UNIT"]),
+                        EXPECTED_DEL_DATE = Convert.ToString(row["EXPECTED_DEL_DATE"]),
+                    });
+                }
+            }
+
+            return _list;
+        }
+
+
+     
+
+    }
+}
---
+++ MaterialController.cs
@@ -0,0 +1,169 @@
+﻿using BusinessLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+using BusinessLayer.DAL;
+using System.Configuration; 
+namespace eSGBIZ.Controllers
+{
+    public class MaterialController : BaseController
+    {
+        public ActionResult MaterialRequisitionEntry()
+        {
+            Material_Entry Entry = new Material_Entry();
+            ViewBag.Header = "Raw Material Requisition Entry";
+
+          
+
+            List<EMPLOYEE_BRACH_ACCESS> _branchList = new DAL_Common().GetEmployeeBrachAccess_List(emp.Employee_Code, "0");
+            Entry.BRANCH_LIST = new SelectList(_branchList, "Branch_Code", "Branch_Name");
+
+            List<MaterialMaster> materialList = new DAL_Common().GetMaterialList();
+            Entry.Material_List = new SelectList(materialList, "Material_Id", "Material_Name");
+
+            List<BrandName_Master> brandNameMasterList = new DAL_Common().GeBrandNameMaster();
+            Entry.BRAND_LIST = new SelectList(brandNameMasterList, "BRAND_ID", "BRAND_NAME");
+
+            
+            return View(Entry);
+        }
+
+        [HttpPost]
+        [SubmitButtonSelector(Name = "Save")]
+        [ActionName("MaterialRequisitionEntry")]
+        public ActionResult MaterialRequisitionEntry_Save(Material_Entry Entry)
+        {
+            var errors = ModelState.Where(x => x.Value.Errors.Count > 0).Select(x => new { x.Key, x.Value.Errors }).ToArray();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ResultMessage objMst;
+                    string result = new DAL_MATERIAL_REQUISITION_ENTRY().INSERT_MATERIAL_REQUISITION_ENTRY(emp.Employee_Code, Entry, out objMst);
+
+                    if (result == "")
+                    {
+                        Success(string.Format("<b>MATERIAL REQUISITION inserted successfully. </b> <b style='color:red'> " + objMst.CODE + "</b>"), true);
+                        return RedirectToAction("MaterialRequisitionEntry", "Material");
+                    }
+                    else
+                    {
+                        Danger(string.Format("<b>Error:</b>" + result), true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Danger(string.Format("<b>Error:</b>" + ex.Message), true);
+                }
+            }
+            else
+            {
+                Danger(string.Format("<b>Error:102 :</b>" + string.Join("; ", ModelState.Values.SelectMany(z => z.Errors).Select(z => z.ErrorMessage))), true);
+            }
+
+
+            List<EMPLOYEE_BRACH_ACCESS> _branchList = new DAL_Common().GetEmployeeBrachAccess_List(emp.Employee_Code, "0");
+            Entry.BRANCH_LIST = new SelectList(_branchList, "Branch_Code", "Branch_Name");
+
+            List<MaterialMaster> materialList = new DAL_Common().GetMaterialList();
+            Entry.Material_List = new SelectList(materialList, "Material_Id", "Material_Name");
+
+            List<BrandName_Master> brandNameMasterList = new DAL_Common().GeBrandNameMaster();
+            Entry.BRAND_LIST = new SelectList(brandNameMasterList, "BRAND_ID", "BRAND_NAME");
+
+            return View(Entry);
+        }
+
+
+
+         [Authorize]
+        public ActionResult MaterialRequisitionList()
+        {
+            ViewBag.Header = "Raw Material Requisition";
+            Material_Req_List Entry = new Material_Req_List();
+
+            List<EMPLOYEE_BRACH_ACCESS> _branchList = new DAL_Common().GetEmployeeBrachAccess_List(emp.Employee_Code, "0");
+            Entry.BRANCH_LIST = new SelectList(_branchList, "Branch_Code", "Branch_Name");
+
+
+            return View(Entry);
+
+        }
+
+    
+        
+        public ActionResult _MaterialRequisitionList()
+        {
+            return PartialView("_MaterialRequisitionList");
+        }
+
+
+        [HttpPost]
+        public ActionResult _Material_Requisition_DataList(string branch_code, DateTime fDate, DateTime tDate)
+        {
+            // Server Side Processing
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+            int totalRow = 0;
+
+            Material_Req_List Entry = new Material_Req_List();
+            List<MATERIAL_REQ_DATA_LIST> materiallist = new List<MATERIAL_REQ_DATA_LIST>();
+            try
+            {
+                if (string.IsNullOrEmpty(branch_code))
+                {
+                    branch_code = "0";
+                }
+                Entry.BRANCH_CODE = branch_code;
+                Entry.From_DT = fDate;
+                Entry.To_DT = tDate;
+
+                materiallist = new DAL_MATERIAL_REQUISITION_ENTRY().Select_Material_Data_List(Entry);
+
+                totalRow = materiallist.Count();
+
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(ex, "Error : _CNs_Data_List ", ex.Message);
+                Danger(string.Format("<b>Exception occured.</b>"), true);
+            }
+
+            if (!string.IsNullOrEmpty(searchValue)) // Filter Operation
+            {
+                materiallist = materiallist.
+                    Where(x => x.REQUISITION_CODE.ToLower().Contains(searchValue.ToLower()) 
+                        || x.REQUISITION_DATE.ToLower().Contains(searchValue.ToLower())
+                        || x.Material_Name.ToLower().Contains(searchValue.ToLower())).ToList<MATERIAL_REQ_DATA_LIST>();
+
+
+
+
+            }
+            int totalRowFilter = materiallist.Count();
+            // sorting
+         
+
+            // Paging
+            if (length == -1)
+            {
+                length = totalRow;
+            }
+            materiallist = materiallist.Skip(start).Take(length).ToList<MATERIAL_REQ_DATA_LIST>();
+
+            var jsonResult = Json(new { data = Entry, draw = Request["draw"], recordsTotal = totalRow, recordsFiltered = totalRowFilter }, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
+        }
+
+      
+
+    }
+}
